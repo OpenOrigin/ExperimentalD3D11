8b934e52c62df75c4a8a6b2a07a5aaff79f94f7b
OpenOrigin <naydenovster@gmail.com>
2015-06-10 01:38:59 -0400

    Removed contrast enhancement in rendering depth buffer

--- Engine/Engine/TexToQuad_PS.hlsl
+++ Engine/Engine/TexToQuad_PS.hlsl
@@ -11,12 +11,8 @@ SamplerState TextureSampler{
 
 Texture2D Texture : register(t0);
 
-float4 P_Shader(InputPixel input) : SV_TARGET{
+float4 P_Shader(InputPixel input) : SV_TARGET {
 	float4 color = Texture.Sample(TextureSampler, input.texUV);
 
-	//color = (2.0f * color) - 1.0f;
-
-	if(color.r <= 0.95f) color.r *= 0.3;
-	
 	return float4(color.r, color.r, color.r, 1.0f);
 }
